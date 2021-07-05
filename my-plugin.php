2e578938ec827de91fa04f012c0669f74cf6f4e4
ahmed <ahmedsport@hotmail.com>
2021-07-05 21:05:59 +0100

    :art: fc :art:

---
+++ my-plugin.php
@@ -0,0 +1,14 @@
+<?php
+/**
+*Plugin Name: my plugin 
+*Description: This is my first plugin! 
+*Author: Ahmed
+*/
+
+// This plugin to display under maintenance page
+function wp_maintenance_mode() {
+    if (!current_user_can('edit_themes') || !is_user_logged_in()) {
+    wp_die('<h1>Under Maintenance</h1><br />Website under planned maintenance. Please check back later.');
+    }
+    }
+    add_action('wp_enqueue_scripts', 'wp_maintenance_mode');
